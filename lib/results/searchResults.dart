324aacda5315a3f9d24e7cb8cf7ed78d0b23506b
Kollipara Sri Krishna Venkata Subbarao <103669180+srikrishna0874@users.noreply.github.com>
2024-03-31 14:57:03 +0530

    UI completed

--- lib/results/searchResults.dart
+++ lib/results/searchResults.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:stock_price_prediction/widgets/inSearchResults.dart';
 
@@ -11,7 +12,6 @@ class SearchResultsPage extends StatefulWidget {
 }
 
 class _SearchResultsPageState extends State<SearchResultsPage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,10 +36,10 @@ class _SearchResultsPageState extends State<SearchResultsPage> {
                 decoration: const BoxDecoration(
                   boxShadow: [
                     BoxShadow(
-                      color: Colors.black,
+                      color: Colors.grey,
                       blurStyle: BlurStyle.outer,
                       blurRadius: 5,
-                    )
+                    ),
                   ],
                 ),
                 child: Text(
@@ -102,7 +102,190 @@ class _SearchResultsPageState extends State<SearchResultsPage> {
                 "ADJ CLOSE",
                 context,
               ),
-
+              SizedBox(
+                height: 18,
+              ),
+              InSearchResults().buildGraphsInSearchResults(
+                  context,
+                  widget.searchResultMap['PREDICTED_IMAGES']['TREND'],
+                  widget.searchResultMap['ticker'],
+                  "RECENT TRENDS IN ${widget.searchResultMap['ticker']} STOCK PRICES"),
+              const SizedBox(
+                height: 30,
+              ),
+              InSearchResults().buildGraphsInSearchResults(
+                context,
+                widget.searchResultMap['PREDICTED_IMAGES']['ARIMA'],
+                widget.searchResultMap['ticker'],
+                "ARIMA MODEL ACCURACY",
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              InSearchResults().buildGraphsInSearchResults(
+                context,
+                widget.searchResultMap['PREDICTED_IMAGES']['LSTM'],
+                widget.searchResultMap['ticker'],
+                "LSTM MODEL ACCURACY",
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              InSearchResults().buildGraphsInSearchResults(
+                context,
+                widget.searchResultMap['PREDICTED_IMAGES']['LINEAR REGRESSION'],
+                widget.searchResultMap['ticker'],
+                "LINEAR REGRESSION MODEL ACCURACY",
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              InSearchResults().buildTomorrowsResults(
+                Color.fromRGBO(0, 123, 255, 1),
+                widget.searchResultMap["ARIMA PREDICTION"].toString(),
+                "TOMORROW'S ${widget.searchResultMap['ticker']} CLOSING PRICE BY ARIMA",
+                context,
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              InSearchResults().buildTomorrowsResults(
+                Color.fromRGBO(255, 193, 7, 1),
+                widget.searchResultMap["LSTM_PREDICTION"].toString(),
+                "TOMORROW'S ${widget.searchResultMap['ticker']} CLOSING PRICE BY LSTM",
+                context,
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              InSearchResults().buildTomorrowsResults(
+                Color.fromRGBO(40, 167, 69, 1),
+                widget.searchResultMap["LINEAR_REGRESSION_PREDICTION"]
+                    .toString(),
+                "TOMORROW'S ${widget.searchResultMap['ticker']} CLOSING PRICE BY LINEAR REGRESSION",
+                context,
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              InSearchResults().buildTomorrowsResults(
+                Color.fromRGBO(220, 53, 69, 1),
+                widget.searchResultMap["ARIMA_ERROR"].toString(),
+                "ARIMA RSE",
+                context,
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              InSearchResults().buildTomorrowsResults(
+                Colors.deepOrange,
+                widget.searchResultMap["LSTM_ERROR"].toString(),
+                "LSTM RSE",
+                context,
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              InSearchResults().buildTomorrowsResults(
+                Colors.deepPurple,
+                widget.searchResultMap["LINEAR_REGRESSION_ERROR"].toString(),
+                "LINEAR REGRESSION RSE",
+                context,
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height * 0.37,
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      blurStyle: BlurStyle.outer,
+                      blurRadius: 2,
+                    ),
+                  ],
+                ),
+                child: Center(child: Text("/* Twitter related UI here....*/")),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height * 0.37,
+                padding: EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey,
+                      blurStyle: BlurStyle.outer,
+                      blurRadius: 2,
+                    ),
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        SizedBox.shrink(),
+                        Text(
+                          "Close",
+                          style: TextStyle(
+                            fontWeight: FontWeight.w800,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Image.asset(
+                          'assets/increase_pic.jpg',
+                          width: 150,
+                          height: 250,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            for (var i in widget
+                                .searchResultMap['NEXT_SEVEN_DAYS_RESULTS'])
+                              Padding(
+                                padding: const EdgeInsets.only(bottom: 5.0),
+                                child: Text(i.toString()),
+                              ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: 110,
+                padding: EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(0, 123, 255, 1),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Text(
+                  "According to the ML Predictions & Sentiment Analysis of the Tweets, a FALL in AAPL stock is expected => SELL",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
             ],
           ),
         ),
--- lib/widgets/inSearchResults.dart
+++ lib/widgets/inSearchResults.dart
@@ -34,4 +34,74 @@ class InSearchResults {
       ),
     );
   }
+
+  Widget buildTomorrowsResults(
+      Color backGroundColor, String value, String key, BuildContext context) {
+    return Container(
+      height: 90,
+      padding: const EdgeInsets.only(right: 20,left: 10),
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        color: backGroundColor,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            value,
+            style: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+          Text(
+            key,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 15,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildGraphsInSearchResults(BuildContext context, String imageUrl,
+      String ticker, String headingText) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height * 0.37,
+      padding: const EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.grey,
+            blurRadius: 2,
+            blurStyle: BlurStyle.outer,
+          ),
+        ],
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Column(
+        children: [
+          Text(
+            headingText,
+            textAlign: TextAlign.left,
+            style: const TextStyle(
+              color: Colors.black,
+              fontSize: 17,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Image.network(imageUrl)
+        ],
+      ),
+    );
+  }
 }
